e32422bae242b1b2030d7cdd307719b7018fdaa9
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-04-27 15:22:58 +0200

    add js istructions (box color with number)

--- script/main.js
+++ script/main.js
@@ -1,1 +1,41 @@
-console.log('js work');
+$(document).ready(function () {
+    
+
+// refs
+
+var callApi = 'https://flynn.boolean.careers/exercises/api/random/int' //api
+var box = $('.wrapper .box'); // box
+
+box.click(function(){
+
+    var self = $(this);
+    //ajax call
+    $.ajax ({
+        url: callApi,
+        method: 'GET',
+        success: function(res) {
+            var number = res.response;
+            console.log(number);
+
+            self.text(number)
+
+            if (number <= 5) {
+                self.addClass('yellow')
+            } else {
+                self.addClass('green')
+            }
+            
+        },
+        error: function() {
+
+        }
+        
+    });
+
+});
+
+
+
+
+
+}); // <-- End Doc Ready
